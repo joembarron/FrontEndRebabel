3ce354f764fa536498b72612fc750bc99da600e0
matthew <mdenslinger@ufl.edu>
2024-09-24 22:03:47 -0400

    Update action function with export command

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [text, setText] = useState("");
-  async function importAction(action) {
+  async function action(action) {
     const response = await window.pythonApi.actions(action);
     setText(() => response);
   }
@@ -12,10 +12,12 @@ function App() {
       <p></p>
       <input type="text" id="text-input" />
       <button id="copyBtn">Copy</button>
-      <button id="importBtn" onClick={() => importAction("import")}>
+      <button id="importBtn" onClick={() => action("import")}>
         Import
       </button>
-      <button id="exportBtn">Export</button>
+      <button id="exportBtn" onClick={() => action("export")}>
+        Export
+      </button>
       <div>
         <h2>Output:</h2>
         <p id="python-text"></p>
