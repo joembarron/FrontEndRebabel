e82e361b0ecab0bc9e99df2fe87622080fdac15c
darrenwang18 <darrenwang2000@gmail.com>
2024-10-01 19:14:53 -0400

    added about text

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,11 +6,19 @@ const About = ({ isOpen, onClose }) => {
       <article>
         <h2>About</h2>
         <p>
-          This is some information on Gap App
+        Linguists use a variety of software tools to document endangered languages. 
+        They frequently need to move language data stored in one program into another, a process for which no automated and user-friendly tool exists. 
+        There is also no efficient way to import language data from the output of natural language processing (NLP) models into these software tools. 
+        The Gap App desktop application will enable linguists to convert between NLP output files and language data file formats used by software like Fieldworks Language Explorer (FLEx) and ELAN. 
+        The app will facilitate the speedy conversion between language data formats through an easy-to-use interface. 
         </p>
+        <h3>Team Members:</h3>
         <ul>
-          <li>Person</li>
-          <li>Person 2</li>
+          <li>Joseph Barron: Backend Developer/Scrum Master</li>
+          <li>Adassa Coimin: Frontend/Backend Developer</li>
+          <li>Matthew Denslinger: Frontend Developer</li>
+          <li>Elizabeth Thorner: Backend Developer/Project Manager</li>
+          <li>Darren Wang: Frontend Developer</li>
         </ul>
         <footer>
           <button className="backBtn" onClick={onClose}>Back</button>
