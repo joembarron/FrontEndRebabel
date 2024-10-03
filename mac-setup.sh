bc60943f52d6cbf2e477417e9c7e8e079688f3f8
Elizabeth Thorner <73670368+ZeldaGaladriel@users.noreply.github.com>
2024-10-03 18:37:18 -0400

    Deleted mac-setup.sh for now

--- mac-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd rebabel_scripts
-python3 -m venv .venv
-/bin/bash ".venv/bin/activate"
-pip install -r requirements.txt
-python -m PyInstaller --onefile --collect-all rebabel_format rebabel_convert.py
-cd dist
-mv rebabel_convert.exe ..
