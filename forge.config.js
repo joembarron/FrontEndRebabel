b978b97f9a4fd3b591c3bf4d25405d68562eb7a5
joembarron <67911762+joembarron@users.noreply.github.com>
2024-10-03 21:17:34 -0400

    Add GitHub publisher in forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -8,21 +8,21 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+      name: '@electron-forge/maker-zip'
     },
+  ],
+  publishers: [
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'joembarron',
+          name: 'github_actions_test'
+        },
+        force: true,
+        prerelease: true,
+      }
+    }
   ],
   plugins: [
     {
