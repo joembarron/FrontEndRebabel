0f1c856bca5b5018538cae0714be6520e1736edf
joembarron <67911762+joembarron@users.noreply.github.com>
2024-10-07 19:40:05 -0400

    Publish Gap App on Mac OS to GitHub releases using GitHub Actions

--- forge.config.js
+++ forge.config.js
@@ -8,21 +8,21 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+      name: '@electron-forge/maker-zip'
     },
+  ],
+  publishers: [
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'ElizabethThorner',
+          name: 'Gap-App'
+        },
+        force: true,
+        prerelease: true,
+      }
+    }
   ],
   plugins: [
     {
