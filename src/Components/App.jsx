5c93a0da1ef6fef00edb0bca5b5dba2037d45c29
matthew <mdenslinger@ufl.edu>
2024-10-10 18:57:55 -0400

    Create isLoading state and add loading spinner

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -20,9 +20,17 @@ function App() {
   const [isHelpOpen, setIsHelpOpen] = useState(false);
   const [isAboutOpen, setIsAboutOpen] = useState(false);
   const [isAddSettingsOpen, setAddSettingsOpen] = useState(false);
+  //Sets loading status for file conversion
+  const [isLoading, setIsLoading] = useState(false);
 
   async function convertFiles() {
+    setIsLoading(true);
     const response = await window.pythonApi.rebabelConvert(data);
+
+    //Temporary setTimeout to show loading
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
   }
 
   async function handleSelectFile() {
@@ -61,11 +69,13 @@ function App() {
             readOnly="readonly"
             placeholder="Select File..."
             value={data.fileName.join(", ")}
+            disabled={isLoading}
           />
           <button
             data-tooltip="Hold Ctrl to Select Multiple Files"
             id="file-in-btn"
             onClick={() => handleSelectFile()}
+            disabled={isLoading}
           >
             Browse
           </button>
@@ -76,6 +86,7 @@ function App() {
             aria-label="Select File Type"
             name="inputType"
             onChange={(e) => handleSelectType(e)}
+            disabled={isLoading}
           >
             <option defaultValue=""></option>
             <option value="flextext">Flextext</option>
@@ -89,6 +100,7 @@ function App() {
             aria-label="Select File Type"
             name="outputType"
             onChange={(e) => handleSelectType(e)}
+            disabled={isLoading}
           >
             <option defaultValue=""></option>
             <option value="flextext">Flextext</option>
@@ -96,15 +108,26 @@ function App() {
           </select>
         </div>
         <div className="settings-container">
-          <button> Mappings</button>
-          <button onClick={() => setAddSettingsOpen(true)}>
+          <button disabled={isLoading}> Mappings</button>
+          <button onClick={() => setAddSettingsOpen(true)} disabled={isLoading}>
             Additional Settings
           </button>
         </div>
         <div className="convert-btn">
-          <button id="convertBtn" onClick={() => convertFiles()}>
-            Convert
-          </button>
+          {!isLoading && (
+            <button
+              id="convertBtn"
+              onClick={() => convertFiles()}
+              disabled={isLoading}
+            >
+              Convert
+            </button>
+          )}
+          {isLoading && (
+            <span className="loading-status" aria-busy="true">
+              Converting...
+            </span>
+          )}
         </div>
       </section>
       {/* Dialog component */}
