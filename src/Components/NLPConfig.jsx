8474468cde657a9e02ecf0d8bcacad024436c01b
matthew <mdenslinger@ufl.edu>
2024-10-14 21:51:14 -0400

    Create base dialog for NLPConfig

--- src/Components/NLPConfig.jsx
+++ src/Components/NLPConfig.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-function NLPConfig() {
-  return <div></div>;
+function NLPConfig({ isOpen, onClose }) {
+  return (
+    <dialog open={isOpen} className="modal-overlay">
+      <article>
+        <h2>NLP Configuration Settings</h2>
+        <hr></hr>
+        <footer>
+          <button onClick={onClose}>Close</button>
+        </footer>
+      </article>
+    </dialog>
+  );
 }
 
 export default NLPConfig;
