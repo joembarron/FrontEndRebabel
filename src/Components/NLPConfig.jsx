b47cb7120607360f27b91f32377395800e2170a7
matthew <mdenslinger@ufl.edu>
2024-10-15 10:48:06 -0400

    Create radio select for nlp file type

--- src/Components/NLPConfig.jsx
+++ src/Components/NLPConfig.jsx
@@ -6,6 +6,15 @@ function NLPConfig({ isOpen, onClose }) {
       <article>
         <h2>NLP Configuration Settings</h2>
         <hr></hr>
+        <section>
+          <fieldset>
+            <legend>NLP File Type:</legend>
+            <input type="radio" id="combined" name="nlp-fileType" />
+            <label htmlFor="combined">Combined</label>
+            <input type="radio" id="separate" name="nlp-fileType" />
+            <label htmlFor="separate">Part of Speech and Language Files</label>
+          </fieldset>
+        </section>
         <footer>
           <button onClick={onClose}>Close</button>
         </footer>
