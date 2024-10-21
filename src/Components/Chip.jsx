24e4d30cd8bb407104d728026110852661d5e020
matthew <mdenslinger@ufl.edu>
2024-10-21 11:34:52 -0400

    Add delete icon

--- src/Components/Chip.jsx
+++ src/Components/Chip.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./Chip.module.css";
+import { TiDelete } from "react-icons/ti";
 
 function Chip({ children }) {
   return (
     <div className={styles.chip}>
       <span className={styles.text}>{children}</span>
+      <TiDelete className={styles.deleteIcon} />
     </div>
   );
 }
