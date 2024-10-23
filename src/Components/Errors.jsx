9027d81efad9138329e0541427be56680900d5c4
matthew <mdenslinger@ufl.edu>
2024-10-23 10:00:35 -0400

    Add custom styling to Errors component

--- src/Components/Errors.jsx
+++ src/Components/Errors.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import styles from "./Errors.module.css";
 
-function Error({ children }) {
-  return <small className={styles.erro}>{children}</small>;
+function Error({ children, customStyle = {} }) {
+  return (
+    <small style={customStyle} className={styles.error}>
+      {children}
+    </small>
+  );
 }
 
 export default Error;
