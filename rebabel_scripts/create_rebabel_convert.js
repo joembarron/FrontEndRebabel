83628737ea71ff768cd563878000d96e5e422b89
darren <darrenwang2000@gmail.com>
2024-10-31 11:19:00 -0400

    change exec to spawn to allow for real time logging

--- rebabel_scripts/create_rebabel_convert.js
+++ rebabel_scripts/create_rebabel_convert.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const path = require('path');
 const isWindows = process.platform === 'win32';
 
@@ -32,19 +32,25 @@ if (isWindows) {
 
     //create executable
     console.log('Executing PowerShell script to create executable...');
-    exec(`powershell.exe -ExecutionPolicy Bypass -File "${scriptPath}"`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing script: ${error.message}`);
+
+    const powershell = spawn('powershell.exe', ['-ExecutionPolicy', 'Bypass', '-File', scriptPath]);
+
+    powershell.stdout.on('data', (data) => {
+        console.log(`PowerShell Output: ${data}`);
+    });
+
+    powershell.stderr.on('data', (data) => {
+        console.error(`PowerShell Error Output: ${data}`);
+    });
+
+    powershell.on('close', (code) => {
+        if (code !== 0) {
+            console.error(`PowerShell script exited with code ${code}`);
             return;
         }
-        if (stderr) {
-            console.error(`Script error: ${stderr}`);
-            return;
-        }
-        console.log(stdout);
         console.log('PowerShell script executed successfully.');
 
-        //copy executable to correct location
+        // Copy executable to correct location
         fs.copyFile(sourcePath, destinationPath, (err) => {
             if (err) {
                 console.error(`Error copying rebabel_convert.exe: ${err.message}`);
