26edefee56c62535d5fdb0dbae1b6de999c54cd0
matthew <mdenslinger@ufl.edu>
2024-11-02 16:42:27 -0400

    Create CSS module for mappings component

--- src/Components/Mappings.jsx
+++ src/Components/Mappings.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Error from "./Error.jsx";
+import styles from "./Mappings.module.css";
 
 function Mappings({ isOpen, onClose, data, setData, errors, setErrorState }) {
   const [isAddTypeMappingOpen, setAddTypeMappingOpen] = useState(false);
@@ -22,9 +23,11 @@ function Mappings({ isOpen, onClose, data, setData, errors, setErrorState }) {
   }
 
   function handleRemoveMapping(mappingTypeIndex, mappingIndex) {
-    const newMappings = data.mappings[mappingTypeIndex].filter((_, i) => i !== mappingIndex);
+    const newMappings = data.mappings[mappingTypeIndex].filter(
+      (_, i) => i !== mappingIndex
+    );
     data.mappings[mappingTypeIndex] = newMappings;
-    if ((data.mappings[0].length === 0) && (data.mappings[1].length === 0)) {
+    if (data.mappings[0].length === 0 && data.mappings[1].length === 0) {
       setErrorState(true, "Mappings must be provided", "mappings");
     }
     setData((data) => ({ ...data }));
@@ -50,85 +53,113 @@ function Mappings({ isOpen, onClose, data, setData, errors, setErrorState }) {
 
   const typeMappings = data.mappings[0].map((pair, index) => {
     return (
-      <li> 
+      <li>
         {pair.in_type} -{">"} {pair.out_type}
-        <button className="remove-button" onClick={() => handleRemoveMapping(0, index)}>Remove</button>
+        <button
+          className={styles.removeButton}
+          onClick={() => handleRemoveMapping(0, index)}
+        >
+          Remove
+        </button>
       </li>
-    )
-  })
+    );
+  });
 
   const featureMappings = data.mappings[1].map((pair, index) => {
     return (
-      <li> 
+      <li>
         {pair.in_feature} -{">"} {pair.out_feature}
-        <button className="remove-button" onClick={() => handleRemoveMapping(1, index)}>Remove</button>
+        <button
+          className={styles.removeButton}
+          onClick={() => handleRemoveMapping(1, index)}
+        >
+          Remove
+        </button>
       </li>
-    )
-  })
+    );
+  });
 
-  const typeDialog = 
-  <dialog open={isAddTypeMappingOpen}>
-    <article>
-      <div className="mappings-container">
-        <div className="input-container left">
-          <label>In Type</label>
-          <input
-            name="in_type"
-            type="text"
-            value={inType}
-            onChange={(e) => handleInputChanges(e)}
-            aria-label="In type settings"
-          />
+  const typeDialog = (
+    <dialog open={isAddTypeMappingOpen}>
+      <article>
+        <div className={styles.mappingsContainer}>
+          <div className={`${styles.inputContainer} ${styles.left}`}>
+            <label>In Type</label>
+            <input
+              name="in_type"
+              type="text"
+              value={inType}
+              onChange={(e) => handleInputChanges(e)}
+              aria-label="In type settings"
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label>Out Type</label>
+            <input
+              name="out_type"
+              type="text"
+              value={outType}
+              onChange={(e) => handleInputChanges(e)}
+              aria-label="Out type settings"
+            />
+          </div>
         </div>
-        <div className="input-container">
-          <label>Out Type</label>
-          <input
-            name="out_type"
-            type="text"
-            value={outType}
-            onChange={(e) => handleInputChanges(e)}
-            aria-label="Out type settings"
-          />
-        </div>
-      </div>
-      <footer>
-          <button onClick={() => data.mappings[0].push({in_type: inType, out_type: outType}) && resetTypeDialog(true)}>Submit</button>
+        <footer>
+          <button
+            onClick={() =>
+              data.mappings[0].push({ in_type: inType, out_type: outType }) &&
+              resetTypeDialog(true)
+            }
+          >
+            Submit
+          </button>
           <button onClick={() => resetTypeDialog(false)}>Cancel</button>
-      </footer>
-    </article>
-  </dialog>
+        </footer>
+      </article>
+    </dialog>
+  );
 
-  const featureDialog = 
-  <dialog open={isAddFeatureMappingOpen}>
-    <article>
-      <div className="mappings-container">
-        <div className="input-container left">
-          <label>In Feature</label>
-          <input
-            name="in_feature"
-            type="text"
-            value={inFeature}
-            onChange={(e) => handleInputChanges(e)}
-            aria-label="In feature settings"
-          />
+  const featureDialog = (
+    <dialog open={isAddFeatureMappingOpen}>
+      <article>
+        <div className={styles.mappingsContainer}>
+          <div className={`${styles.inputContainer} ${styles.left}`}>
+            <label>In Feature</label>
+            <input
+              name="in_feature"
+              type="text"
+              value={inFeature}
+              onChange={(e) => handleInputChanges(e)}
+              aria-label="In feature settings"
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label>Out Feature</label>
+            <input
+              name="out_feature"
+              type="text"
+              value={outFeature}
+              onChange={(e) => handleInputChanges(e)}
+              aria-label="Out feature settings"
+            />
+          </div>
         </div>
-        <div className="input-container">
-          <label>Out Feature</label>
-          <input
-            name="out_feature"
-            type="text"
-            value={outFeature}
-            onChange={(e) => handleInputChanges(e)}
-            aria-label="Out feature settings"
-          />
-        </div>
-      </div>
-      <footer>
-        <button onClick={() => data.mappings[1].push({in_feature: inFeature, out_feature: outFeature}) && resetFeatureDialog(true)}>Submit</button>
-        <button onClick={() => resetFeatureDialog(false)}>Cancel</button>
-      </footer>
-    </article>
-  </dialog>
+        <footer>
+          <button
+            onClick={() =>
+              data.mappings[1].push({
+                in_feature: inFeature,
+                out_feature: outFeature,
+              }) && resetFeatureDialog(true)
+            }
+          >
+            Submit
+          </button>
+          <button onClick={() => resetFeatureDialog(false)}>Cancel</button>
+        </footer>
+      </article>
+    </dialog>
+  );
 
   return (
     <dialog id="a" open={isOpen} className="modal-overlay">
@@ -137,26 +168,20 @@ function Mappings({ isOpen, onClose, data, setData, errors, setErrorState }) {
         <hr></hr>
         <div>
           <label>Current Type Mappings:</label>
-          <ul>
-            {typeMappings}
-          </ul>
+          <ul>{typeMappings}</ul>
           <label>Current Feature Mappings:</label>
-          <ul>
-            {featureMappings}
-          </ul>
+          <ul>{featureMappings}</ul>
         </div>
-        <section id="mappings-inputs">
-          <div className="mappings-container">
-            <div className="type-button-container">
-              <button
-                onClick={() => setAddTypeMappingOpen(true)}>
-                  Add New Type Mapping
+        <section>
+          <div className={styles.mappingsContainer}>
+            <div className={styles.typeButtonContainer}>
+              <button onClick={() => setAddTypeMappingOpen(true)}>
+                Add New Type Mapping
               </button>
               {typeDialog}
             </div>
-            <div className="input-container">
-              <button 
-                onClick={() => setAddFeatureMappingOpen(true)}>
+            <div className={styles.inputContainer}>
+              <button onClick={() => setAddFeatureMappingOpen(true)}>
                 Add New Feature Mapping
               </button>
               {featureDialog}
